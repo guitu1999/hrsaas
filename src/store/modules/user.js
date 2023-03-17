6493306fb0b38a197ab98dd70b9529a1fdad7b80
guitu <95765497@qq.com>
2023-03-17 14:33:52 +0800

    封装登录的action 存取token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,37 @@
+import { getToken, setToken, removeToken } from '@/utils/auth.js'
+import { login } from '@/api/user'
+const state = {
+  // 每次登录先从缓存中读取token
+  token: getToken() || null // 登录的etoken
+}
+// 同步方法
+const mutations = {
+  // 修改token
+  SetToken(state, token) {
+    state.token = token // 同步到vuex
+    setToken(token) // 同步到缓存
+  },
+  // 删除token
+  removeToken(state) {
+    state.token = '' // 清空vuex
+    removeToken() // 清空缓存
+  }
+}
+// 异步方法
+const actions = {
+  // 登录方法
+  async loginAsync(context, data) {
+    // 调用登录接口
+    const result = await login(data)
+    if (result.data.success) {
+      console.log('登录接口请求成功', result)
+      context.commit('SetToken', result.data.data) // 调用同步更改token方法
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  mutations,
+  actions
 }
