f26d002353d8630d3225c245ccd99f685a9c94fd
guitu <95765497@qq.com>
2023-03-17 11:44:15 +0800

    Vue-cl配置跨域代理

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,15 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    // 代理跨域的配置
+    proxy: {
+      // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
+      '/api': {
+        target: 'http://ihrm.itheima.net/', // 跨域请求的地址 此服务器管理员密码禁止修改
+        // 另外一个服务器地址：http://ihrm-java.itheima.net/
+        changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+      }
     }
   },
   configureWebpack: {
