1fbbfdd630a68ba1a1c094fc13643b2393237063
guitu <95765497@qq.com>
2023-03-20 10:14:31 +0800

    封装获取用户资料的action 并 getters共享用户状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,9 @@
 import { getToken, setToken, removeToken } from '@/utils/auth.js'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 const state = {
   // 每次登录先从缓存中读取token
-  token: getToken() || null // 登录的etoken
+  token: getToken() || null, // 登录的etoken
+  userInfo: {} // 用户信息
 }
 // 同步方法
 const mutations = {
@@ -15,6 +16,14 @@ const mutations = {
   removeToken(state) {
     state.token = '' // 清空vuex
     removeToken() // 清空缓存
+  },
+  // 用户信息
+  getInfo(state, data) {
+    state.userInfo = data
+  },
+  // 删除用户信息
+  removeInfo(state) {
+    state.userInfo = {}
   }
 }
 // 异步方法
@@ -25,6 +34,13 @@ const actions = {
     const result = await login(data)
     console.log('登录接口请求成功', result)
     context.commit('SetToken', result) // 调用同步更改token方法
+  },
+  // 获取用户信息方法
+  async getUserInfoAsync(context) {
+    const result = await getUserInfo()
+    console.log('用户信息接口请求成功', result)
+    context.commit('getInfo', result) // 调用同步方法
+    return result // 返回   给我们后期做权限的伏笔
   }
 }
 export default {
