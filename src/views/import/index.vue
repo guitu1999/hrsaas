44ec70e374ff48e0b8805b271375e1cace806efb
guitu <95765497@qq.com>
2023-03-24 21:44:05 +0800

    处理excel 导入时的时间问题

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -33,7 +33,12 @@ export default {
       const newarray = results.map(item => {
         const form = {}
         Object.keys(item).forEach(key => {
-          form[userRelations[key]] = item[key]
+          if (userRelations[key] === 'timeOfEntry' || userRelations[key] === 'correctionTime') {
+            // 因为后台要求的是时间形式   需要使用new Date
+            form[userRelations[key]] = new Date(this.formatDate(item[key], '/'))
+          } else {
+            form[userRelations[key]] = item[key]
+          }
         })
         return form
         // console.log('打印', Object.keys(item))
@@ -42,6 +47,18 @@ export default {
       await importEmployee(newarray)// 调用接口
       this.$message.success('导入成功')
       this.$router.back() // 回退上一个页面
+    },
+    // excel 处理时间
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)
     }
   }
 }
