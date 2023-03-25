1b1aa3912b4781e33cb84cdc523ec6d897622311
guitu <95765497@qq.com>
2023-03-25 19:53:33 +0800

    计算属性根据图片数量 判断是否显示上传图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <!-- action 为要上传到的地方 -->
-    <el-upload :limit="1" :file-list="fileList" list-type="picture-card" action="#" :on-preview="previewImg">
+    <el-upload :class="{ showImg: fileComputed }" :limit="1" :file-list="fileList" list-type="picture-card" action="#"
+      :on-preview="previewImg">
       <i class="el-icon-plus" />
     </el-upload>
     <el-dialog title="图片预览" :visible.sync="dialogVisible">
@@ -19,6 +20,13 @@ export default {
       fileList: [{ url: 'https://img2.baidu.com/it/u=3618236253,1028428296&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1679850000&t=1267ebdc06f172b2db194aa8c92400e2' }]
     }
   },
+  // 计算属性
+  computed: {
+    // 判断是否显示图片上传
+    fileComputed() {
+      return this.fileList.length === 1
+    }
+  },
   methods: {
     // 预览图片
     previewImg(file) {
@@ -30,4 +38,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.showImg .el-upload--picture-card {
+  display: none;
+}
+</style>
