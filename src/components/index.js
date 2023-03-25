444ee5668e35bee3dc550c40a21bb0cec38ee0b9
guitu <95765497@qq.com>
2023-03-25 19:06:01 +0800

    上传图片的封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import PageTools from './PageTools'// 公共的头部组件
 import UploadExcel from './UploadExcel'// 导入excel组件
+import ImageUpload from './ImageUpload' // 导入图片上传组件
 // 导出
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
