def8177873f585b7dfbf5c930b669629827586ba
guitu <95765497@qq.com>
2023-03-25 19:05:35 +0800

    上传图片组件的疯转

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-upload list-type="picture-card">
+    <i class="el-icon-plus" />
+  </el-upload>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
