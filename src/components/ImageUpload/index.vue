eb1d38ee088db6d0d8f4c2ba12ab270fc8f87823
guitu <95765497@qq.com>
2023-03-25 19:35:37 +0800

    点击图片进行预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <el-upload list-type="picture-card">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <!-- action 为要上传到的地方 -->
+    <el-upload :limit="1" :file-list="fileList" list-type="picture-card" action="#" :on-preview="previewImg">
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog title="图片预览" :visible.sync="dialogVisible">
+      <img :src="imgUrl" style="width: 100%;">
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      imgUrl: '', // 图片地址
+      dialogVisible: false, // 图片预览弹窗
+      fileList: [{ url: 'https://img2.baidu.com/it/u=3618236253,1028428296&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1679850000&t=1267ebdc06f172b2db194aa8c92400e2' }]
+    }
+  },
+  methods: {
+    // 预览图片
+    previewImg(file) {
+      console.log('file', file)
+      this.imgUrl = file.url
+      this.dialogVisible = true
+    }
+  }
 }
 </script>
 
