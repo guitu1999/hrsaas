289c26dcf08fbf646a856701067f1de71cdbab2e
guitu <95765497@qq.com>
2023-03-26 21:09:49 +0800

    新建权限管理页面  接口

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <!-- 头部组件 -->
+      <PageTools>
+        <el-button slot="right" type="primary" size="small">
+          添加权限
+        </el-button>
+      </PageTools>
+      <!-- 表格 -->
+      <el-table border>
+        <el-table-column align="center" label="名称" />
+        <el-table-column align="center" label="标识" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+
+      </el-table>
     </div>
   </div>
 </template>
