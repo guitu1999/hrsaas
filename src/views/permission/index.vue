2683b7ade54e5cb3bef95cdaa6b99815f03e220e
guitu <95765497@qq.com>
2023-03-27 09:52:24 +0800

    获取权限数据并转换成树形

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -8,26 +8,41 @@
         </el-button>
       </PageTools>
       <!-- 表格 -->
-      <el-table border>
-        <el-table-column align="center" label="名称" />
-        <el-table-column align="center" label="标识" />
-        <el-table-column align="center" label="描述" />
-        <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="text">添加</el-button>
-            <el-button type="text">编辑</el-button>
-            <el-button type="text">删除</el-button>
+      <el-table :data="list" border="" row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="标识" prop="code" />
+        <el-table-column label="描述" prop="description" />
+        <el-table-column label="操作">
+          <template slot-scope="{ row }">
+            <el-button v-if="row.type === 1" type="text" @click="addPermission(2, row.id)">添加</el-button>
+            <el-button type="text" @click="editPermission(row.id)">编辑</el-button>
+            <el-button type="text" @click="delPermission(row.id)"> 删除</el-button>
           </template>
         </el-table-column>
-
       </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils/index'
 export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    // 获取权限列表
+    async getPermissionList() {
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+    }
 
+  }
 }
 </script>
 
