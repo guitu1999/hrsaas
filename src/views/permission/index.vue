e894d1f16601a06833f39a8d66c0a0ebeb29d00a
guitu <95765497@qq.com>
2023-03-27 10:05:39 +0800

    权限添加编辑的创建

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -20,6 +20,30 @@
           </template>
         </el-table-column>
       </el-table>
+      <!-- 放置一个弹层 用来编辑新增节点 -->
+      <el-dialog title="新增权限" :visible="showDialog" @close="btnCancel">
+        <!-- 表单 -->
+        <el-form ref=" permForm" :model="formData" :rules="rules" label-width="120px">
+          <el-form-item label="权限名称" prop="name">
+            <el-input v-model="formData.name" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限标识" prop="code">
+            <el-input v-model="formData.code" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch v-model="formData.enVisible" active-value="1" inactive-value="0" />
+          </el-form-item>
+        </el-form>
+        <el-row slot="footer" type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+            <el-button size="small" @click="btnCancel">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -30,7 +54,20 @@ import { tranListToTreeData } from '@/utils/index'
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      },
+      rules: {
+        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+      },
+      showDialog: true
     }
   },
   created() {
@@ -40,8 +77,15 @@ export default {
     // 获取权限列表
     async getPermissionList() {
       this.list = tranListToTreeData(await getPermissionList(), '0')
-    }
+    },
+    // 确定
+    btnOK() {
 
+    },
+    // 取消
+    btnCancel() {
+
+    }
   }
 }
 </script>
